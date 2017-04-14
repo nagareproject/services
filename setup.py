ef6e1422626cba3673997b43bb1f458004f5bf0d
Alain Poirier <alain.poirier@net-ng.com>
2017-04-14 15:12:53 +0200

    2017 header

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # Encoding: utf-8
 
-#=-
-# (C)opyright Net-ng 2012-2015
+# =-
+# (C)opyright Net-ng 2012-2017
 #
 # This is a Net-ng source code.
 # Any reproduction modification or use without prior written
 # approval from Net-ng is strictly forbidden.
-#=-
+# =-
 
 import os
 
