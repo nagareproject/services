36900d6712705da3a6b14e548edd48d1b8a10bc4
Alain Poirier <alain.poirier@net-ng.com>
2021-07-27 16:44:39 +0200

    Service names conversion to Python identifiers

--- nagare/services/plugins.py
+++ nagare/services/plugins.py
@@ -124,7 +124,7 @@ class Plugins(object):
                 plugin_config = config.get(name, {})
                 plugin_instance = self._load_plugin(name, entry.dist, plugin, **plugin_config)
                 if plugin_instance is not None:
-                    self[name] = plugin_instance
+                    self[name.replace('.', '_')] = plugin_instance
             except Exception:
                 print("'%s' can't be loaded" % name)
                 raise
